d07d8593171e1164bab72f33ab5d8c076121780b
Waminms <67516571+Waminms@users.noreply.github.com>
2023-02-19 15:14:16 +0200

    Compose added

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,12 @@
+import java.util.Locale;
 import java.util.function.Function;
 
 public class Main {
     public static void main(String[] args) {
         String str = "Hello 2 worlD++";
-        Function<String, Integer> fun1 = a -> countLetters(str);
-        System.out.println(fun1.apply(str));
-    }
-
-    public static Integer countLetters(String str){
-        String regex = "[^bcdfghjklmnpqrstvwxz]";
-        String strResult = str.toLowerCase().replaceAll(regex, "");
-        return strResult.length();
+        Function<String, String> fun1 = a -> a.toLowerCase().replaceAll("[^bcdfghjklmnpqrstvwxz]", "");
+        Function<String, Integer> fun2 = a -> a.length();
+        Function<String, Integer> fun3 = fun2.compose(fun1);
+        System.out.println(fun3.apply(str));
     }
 }
